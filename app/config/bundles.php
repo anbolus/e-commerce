f32ba6421628970033cba2e37d8a65ae249f4ad2
Andrea Bolus <andrea.bolus@epitech.eu>
2023-04-25 11:00:18 +0200

    Revert "product entity"
    This reverts commit 70cced54bb4289eb5c908a5082aa349d78f3dfe6.

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
